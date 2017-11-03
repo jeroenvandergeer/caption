cc08296cc01bfa1b286516ec814ab3d82625868b
Giel Cobben <g.cobben@gmail.com>
2017-11-03 14:29:55 +0100

    Removed fs from renderer

--- renderer/utils/index.js
+++ renderer/utils/index.js
@@ -1,5 +1,4 @@
 // Packages
-import fs from "fs";
 import movieExtentsion from "../data/extenstions";
 
 // Process files
@@ -18,7 +17,7 @@ const processFiles = rawFiles => {
       size: file.size,
       name: file.name,
       path: file.path,
-      status: "loading"
+      status: "loading",
     };
 
     files.push(fileObject);
@@ -30,14 +29,13 @@ const processFiles = rawFiles => {
 // File size readable
 const fileSizeReadable = size => {
   if (size >= 1000000000) {
-    return Math.ceil(size / 1000000000) + "GB";
+    return `${Math.ceil(size / 1000000000)}GB`;
   } else if (size >= 1000000) {
-    return Math.ceil(size / 1000000) + "MB";
+    return `${Math.ceil(size / 1000000)}MB`;
   } else if (size >= 1000) {
-    return Math.ceil(size / 1000) + "kB";
-  } else {
-    return Math.ceil(size) + "B";
+    return `${Math.ceil(size / 1000)}kB`;
   }
+  return `${Math.ceil(size)}B`;
 };
 
 // Export
